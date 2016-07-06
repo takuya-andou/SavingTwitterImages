2f2e56e184426c272bfff73680fe4d3ba2597283
Ando <a@MacBook-Pro-2.local>
2016-07-06 22:03:03 +0900

    FirstPush 日本語のツイートを垂れ流して表示するだけ

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+ 
+import json, urllib2, oauth2 as oauth
+import ConfigParser
+
+inifile = ConfigParser.SafeConfigParser()
+inifile.read("./config.ini")
+
+
+api_key = inifile.get("twitter","api_key")
+api_secret = inifile.get("twitter","api_secret")
+access_key = inifile.get("twitter","access_token")
+access_secret = inifile.get("twitter","access_secret")
+
+
+consumer = oauth.Consumer(api_key, api_secret)
+token = oauth.Token(access_key, access_secret)
+ 
+#エンドポイントURL
+#https://dev.twitter.com/docs/api/1.1/get/statuses/sample あたり参照
+#trackなどの条件を指定して絞り込むのも好いかも
+url = 'https://stream.twitter.com/1.1/statuses/sample.json'
+ 
+request = oauth.Request.from_consumer_and_token(consumer, token, http_url=url)
+request.sign_request(oauth.SignatureMethod_HMAC_SHA1(), consumer, token)
+res = urllib2.urlopen(request.to_url())
+ 
+for r in res:
+    data = json.loads(r)
+ 
+    try:
+        #日本語のツイートだけ表示
+        if data['user']['lang'] == 'ja':
+            print data['user']['screen_name']
+            print data['text']
+            print
+ 
+    except:
+        #たまーにデリートフラグのついたツイートが流れてくるので（？）適当に受け流す 詳しくは未調査
+        pass
